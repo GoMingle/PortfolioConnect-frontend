ca6ca54ba89219a645d7dd55526df00e61feade8
Agnes Odame <agnesodame5@gmail.com>
2024-07-11 16:26:26 +0000

    Signup and login pages updated

--- src/pages/logIn.jsx
+++ src/pages/logIn.jsx
@@ -1,62 +1,78 @@
+import { useForm } from "react-hook-form";
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const LogIn = () => {
 
-    const [showPassword, setShowPassword] = useState(false);
-    const togglePasswordVisibility = () => {
-        setShowPassword(!showPassword);
-      };
-    return (
-         <div>
-         <div className="flex items-center justify-center min-h-screen bg-gray-900">
-        <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
-          <h2 className="text-2xl font-bold text-center">Log In</h2>
-             <form className="space-y-6">
-             <div>
-             <label htmlFor="email" className="block text-sm font-medium text-gray-700"></label>
-            <input
-                    type="email"
-                     id="email"
-                     name="email"
-                     placeholder="Email"
-                     required
-                    className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring focus:ring-indigo-200"
-                 />
-                 </div>
-                 <div className="relative">
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700"></label>
-            <input
-              type={showPassword ? "text" : "password"}
-              id="password"
-              name="password"
-              required
-              placeholder="password"
-              className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring focus:ring-indigo-200"
-            />
-            <span
-              className="absolute inset-y-0 right-0 flex items-center px-2 cursor-pointer"
-              onClick={togglePasswordVisibility}
-            >
-              <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} className='mt-3 size-3' />
-            </span>
-          </div>
-                  <button
-            type="submit"
-            className="w-full px-4 py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700"
-          >
-            Login
-          </button>
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
-                 </form>
-                 <p className="text-center text-sm text-gray-600">
-          Forgot <a href="/login" className="text-indigo-600 hover:underline">Password?</a>
-        </p>
-                </div>
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-teal-400 to-gray-900 ">
+        <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+          <h2 className="text-2xl font-bold text-center">Welcome Back</h2>
+          <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700"></label>
+              <input
+                type="email"
+                id="email"
+
+                placeholder=" Enter your Email"
+
+                className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring  focus:ring-teal-400"
+                {
+                ...register("email", { required: "Email is required" })
+                }
+              />
+              {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
             </div>
+            <div className="relative">
+              <label htmlFor="password" className="block text-sm font-medium text-gray-700"></label>
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                placeholder=" Enter your password"
+                className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring  focus:ring-teal-400"
+                {
+                  ...register("password", { required: "Password is required" })
+                  }
+              />
+              {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+              <span
+                className="absolute inset-y-0 right-0 flex items-center px-2 cursor-pointer"
+                onClick={togglePasswordVisibility}
+              >
+                <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} className='mt-3 size-3' />
+              </span>
+            </div>
+            <button
+              type="submit"
+              className="w-full px-4 py-2 text-white  rounded-md bg-teal-400"
+            >
+              Login
+            </button>
+
+          </form>
+          <p className="text-center text-sm text-gray-600">
+            Forgot <a href="/login" className="text-teal-400 hover:underline">Password?</a>
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default LogIn
--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,105 +1,133 @@
-
+import { useForm } from "react-hook-form";
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import signUpImage from "../assets/images/signUpImage.png" ;
 
 const SignUp = () => {
-    const [showPassword, setShowPassword] = useState(false);
-    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-    const togglePasswordVisibility = () => {
-        setShowPassword(!showPassword);
-      };
-    
-      const toggleConfirmPasswordVisibility = () => {
-        setShowConfirmPassword(!showConfirmPassword);
-      };
-  
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
-        <div className="flex items-center justify-center min-h-screen bg-gray-900">
-      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold text-center">Create Account</h2>
-        <form className="space-y-6">
-          <div>
-            <label htmlFor="name" className="block text-sm font-medium text-gray-700"></label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              required
-              placeholder="Username"
-              className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring  focus:ring-teal-400"
-            />
-          </div>
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700"></label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Email"
-              required
-              className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring  focus:ring-teal-400"
-            />
-          </div>
-          <div className="relative">
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700"></label>
-            <input
-              type={showPassword ? "text" : "password"}
-              id="password"
-              name="password"
-              required
-              placeholder="password"
-              className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring  focus:ring-teal-400"
-            />
-            <span
-              className="absolute inset-y-0 right-0 flex items-center px-2 cursor-pointer"
-              onClick={togglePasswordVisibility}
+      <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-teal-400 to-gray-900 ">
+        <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+          <h2 className="text-2xl font-bold text-center">Create Account</h2>
+
+          <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <label htmlFor="name" className="block text-sm font-medium text-gray-700">UserName</label>
+              <input
+                type="text"
+                id="userName"
+               
+                placeholder="Enter your Username"
+                className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring  focus:ring-teal-400"
+                {
+                ...register("userName", { required: "Username is required" })
+                }
+              />
+               {errors.userName && (
+              <p className="text-red-500">{errors.userName.message}</p>
+            )}
+            </div>
+
+            
+
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder=" Enter your Email"
+            
+                className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring  focus:ring-teal-400"
+                {
+                ...register("email", { required: "Email is required" })
+                }
+              />
+               {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
+            </div>
+            <div className="relative">
+              <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                placeholder="password"
+                className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring  focus:ring-teal-400"
+                {
+                ...register("password", { required: "Password is required" })
+                }
+              />
+              {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+              <span
+                className="absolute inset-y-0 right-0 flex items-center px-2 cursor-pointer"
+                onClick={togglePasswordVisibility}
+              >
+                <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} className='mt-5 size-3' />
+              </span>
+            </div>
+            <div className="relative">
+              <label htmlFor="confirm-password" className="block text-sm font-medium text-gray-700">Confirm Password</label>
+              <input
+                type={showConfirmPassword ? "text" : "password"}
+                id="confirmPassword"
+                placeholder="confirm-password"
+                className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring  focus:ring-teal-400"
+                {
+                ...register("confirmPassword", { required: "Confirm Password is required" })
+                }
+              />
+              {errors.confirmPassword && (
+              <p className="text-red-500">{errors.confirmPassword.message}</p>
+            )}
+              <span
+                className="absolute inset-y-0 right-0 flex items-center px-2 cursor-pointer"
+                onClick={toggleConfirmPasswordVisibility}
+              >
+                <FontAwesomeIcon icon={showConfirmPassword ? faEyeSlash : faEye} className='mt-5 size-3' />
+              </span>
+            </div>
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                id="terms"
+                name="terms"
+          
+                className="w-4 h-4 "
+              />
+              <label htmlFor="terms" className="ml-2 text-sm text-gray-700">I agree to the <a href="#" className="text-teal-400 hover:underline">Terms of Service</a></label>
+            </div>
+            <button
+              type="submit"
+              className="w-full px-4 py-2 text-white bg-teal-400 rounded-md"
             >
-              <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} className='mt-5 size-3' />
-            </span>
-          </div>
-          <div className="relative">
-            <label htmlFor="confirm-password" className="block text-sm font-medium text-gray-700"></label>
-            <input
-              type={showConfirmPassword ? "text" : "password"}
-              id="confirm-password"
-              name="confirm-password"
-              required
-              placeholder="confirm-password"
-              className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring  focus:ring-teal-400"
-            />
-            <span
-              className="absolute inset-y-0 right-0 flex items-center px-2 cursor-pointer"
-              onClick={toggleConfirmPasswordVisibility}
-            >
-              <FontAwesomeIcon icon={showConfirmPassword ? faEyeSlash : faEye} className='mt-5 size-3'   />
-            </span>
-          </div>
-          <div className="flex items-center">
-            <input
-              type="checkbox"
-              id="terms"
-              name="terms"
-              required
-              className="w-4 h-4 "
-            />
-            <label htmlFor="terms" className="ml-2 text-sm text-gray-700">I agree to the <a href="#" className="text-teal-400 hover:underline">Terms of Service</a></label>
-          </div>
-          <button
-            type="submit"
-            className="w-full px-4 py-2 text-white bg-teal-400 rounded-md"
-          >
-            Sign Up
-          </button>
-        </form>
-        <p className="text-center text-sm text-gray-600">
-          Have already an account? <a href="/login" className="text-teal-400 hover:underline">Login here</a>
-        </p>
-        
+              Sign Up
+            </button>
+          </form>
+          <p className="text-center text-sm text-gray-600">
+            Have already an account? <a href="/login" className="text-teal-400 hover:underline">Login here</a>
+          </p>
+
+        </div>
       </div>
-    </div>
     </div>
   )
 }
