2a866a0fb6c3ea66eddb4836c0414b51290db45f
ella-quintin <equintin41@gmail.com>
2024-07-11 21:17:25 +0000

    Landing page updated as requested.

---
+++ src/constants/index.jsx
@@ -0,0 +1,27 @@
+import { ShareIcon } from "@heroicons/react/24/outline"
+import { FilePlus2, Share2Icon, ViewIcon } from "lucide-react"
+
+const K = {
+FEATURE:[
+{
+    title: "Create portfolio",
+description: "This allows users to showcase their projects, achievements, and experiences in a visually appealing way.",
+Icons: <FilePlus2 size={24}/>,
+iconBg: "#F2F2F2",
+},
+{
+    title: "Preview",
+description: "Allows users to review and visualize their portfolio before publishing or sharing it with others.",
+Icons: <ViewIcon size={24} />,
+iconBg: "#F2F2F2",
+},
+{
+    title: "Share/connect",
+description: "Enables users to share their portfolio with others and connect with like-minded individuals and potential employers. ",
+Icons: <Share2Icon size={24}/>,
+iconBg: "#F2F2F2",
+},
+],
+}
+
+export default K
