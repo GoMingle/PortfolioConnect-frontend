229b86565ebfd03c5422e3b5fe384516cdbb0360
Agnes Odame <agnesodame5@gmail.com>
2024-07-17 17:55:07 +0000

    make changes to the dashboard ui

--- src/pages/dashboard/layout/pagesLayout.jsx
+++ src/pages/dashboard/layout/pagesLayout.jsx
@@ -4,8 +4,8 @@ const PagesLayout = ({ headerText, buttonText, children, onClick }) => {
   return (
     <div className="p-10 flex flex-col gap-y-16 ">
       <div className="flex mt-6">
-        <h1 className="text-3xl font-bold text-gray-800">{headerText}</h1>
-        <button className="bg-transparent border-2 border-gray-800 hover:bg-gray-800 text-white font-bold   focus:outline-none focus:shadow-outline px-6 py-2 ml-auto rounded-lg flex gap-x-2" onClick={onClick}>
+        <h1 className="text-3xl font-bold text-white">{headerText}</h1>
+        <button className="   hover:bg-gray-700 bg-gray-800 text-white font-bold   focus:outline-none focus:shadow-outline px-6 py-2 ml-auto rounded-lg flex gap-x-2" onClick={onClick}>
           <PlusCircleIcon />
           {buttonText}
         </button>
